5000c48093b922e7f45cc2345aebfcf5c4ba2627
Daniela Perez <daniela@correo.io>
2016-10-16 22:36:54 -0500

    agrega git al script windows_10_daniela.ps1

--- powershell/windows_10_daniela.ps1
+++ powershell/windows_10_daniela.ps1
@@ -6,7 +6,7 @@
 #[System.IO.Directory]::CreateDirectory("c:\daniela\priscila\tu_tio")
 
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y skype spotify songr teamviewer firefox
-
+choco install -y skype spotify songr teamviewer firefox git
+ 
 #2. descargar e instalar software pirata
 #3. descargar e instalar la ayuda de powershell
