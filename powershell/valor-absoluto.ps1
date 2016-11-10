5cdc29a3595000a987c996a4e25ad68dbef20efb
Daniela Perez <daniela@correo.io>
2016-11-09 19:37:30 -0600

    Powershell: agrega scripts y tests

---
+++ powershell/valor-absoluto.ps1
@@ -0,0 +1,10 @@
+﻿function valor-absoluto($numero) {
+    if($numero) {
+        if($numero -is [int]) {
+            if($numero -lt 0) {
+               $numero = $numero * -1;
+            }
+            "$numero";
+        }
+    }
+}
