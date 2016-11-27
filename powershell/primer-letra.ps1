5484710a2d305d31086b80d35ce2dda29c218c7c
Daniela Perez <daniela@correo.io>
2016-11-26 22:31:11 -0600

    Implementacion de la funcion primer letra

---
+++ powershell/primer-letra.ps1
@@ -0,0 +1,9 @@
+﻿function primer-letra () {
+    ForEach ($argumento in $args) {
+        $argumento = [String]$argumento;
+        $caracteres_en_el_argumento = $argumento.tochararray();
+        $resultado = $caracteres_en_el_argumento[0];
+        break;
+    }
+    "$resultado";
+}
