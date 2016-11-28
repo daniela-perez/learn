f0da891d0f61de219a77cd2a8a91e98d57f25af9
Daniela Perez <daniela@correo.io>
2016-11-27 21:00:26 -0600

    Agrega nuevo test, contar-palabras-tests.ps1

---
+++ powershell/tests/contar-palabras.tests.ps1
@@ -0,0 +1,24 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut  = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".tests.", ".")
+. "$here\..\$sut"
+
+Describe "contar-palabras" {
+    BeforeEach {
+        "hola mundo cruel" > $here\contar-palabras.1
+        "Git is the most popular version control system that developers use to track and share code" > $here\contar-palabras.2
+    }
+
+    AfterEach {
+        Remove-Item $here\contar-palabras.1
+        Remove-Item $here\contar-palabras.2
+        Remove-Item $here\contar-palabras.3
+    }
+
+    It "contar-palabras" {
+        contar-palabras $here\contar-palabras.1 | Should Be '3'
+        "Hola mundo cruel" | contar-palabras | Should be '3'
+        contar-palabras $here\contar-palabras.2 | Should Be '16'
+        contar-palabras $here\contar-palabras.1 $here\contar-palabras.2 | Should Be '19'
+        "Hola mundo cruel" | contar-palabras $here\contar-palabras.1 | Should Be '6'
+    }
+}
