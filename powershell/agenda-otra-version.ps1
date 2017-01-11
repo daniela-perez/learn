9097f436eb6715dc29a56861c363b9e581faa65f
Daniela Perez <daniela@correo.io>
2017-01-10 22:19:45 -0600

    Implementacion de la funcion agenda-otra-version

---
+++ powershell/agenda-otra-version.ps1
@@ -0,0 +1,28 @@
+﻿function agenda-otra-version() {    
+    $brenda = "brenda"
+    $brenda_informacion =  "Celular: 5531400539, E-mail: brenzulo.0204@gmail.com"
+    $constantino = "constantino"
+    $constantino_informacion = "Casa: 57368170"
+    $daniela = "daniela"
+    $daniela_informacion = "Celular: 5565311676, E-mail: daniela@correo.io"
+
+    $nombre_escrito_por_el_usuario = $args
+
+    if ($nombre_escrito_por_el_usuario -ieq "$brenda") { 
+        Write-Output "$brenda_informacion"
+    }
+
+    elseif ($nombre_escrito_por_el_usuario -ieq $constantino) { 
+        Write-Output "$constantino_informacion"
+    }
+
+    elseif ($nombre_escrito_por_el_usuario -ieq $daniela) { 
+        Write-Output "$daniela_informacion"
+    }
+
+    else {
+        Write-Output "El nombre `'$nombre_escrito_por_el_usuario`' no esta incluido en la agenda"; return
+    }
+
+    "Si deseas conocer la informacion de otra persona, escribe su nombre."
+}
---
+++ powershell/tests/agenda-otra-version.tests.ps1
@@ -0,0 +1,15 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut  = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".tests.", ".")
+. "$here\..\$sut"
+
+Describe "agenda-otra-version" {
+    It "agenda-otra-version" {
+        agenda-otra-version brenda  | select -Last 3 | select -First 1 | Should Be 'Celular: 5531400539, E-mail: brenzulo.0204@gmail.com'
+        agenda-otra-version brenda  | select -Last 1 | Should Be 'Si deseas conocer la informacion de otra persona, escribe su nombre.'
+        agenda-otra-version daniela | select -Last 2 | select -First 1 | Should Be 'Celular: 5565311676, E-mail: daniela@correo.io'
+        agenda-otra-version constantino | select -Last 2 | select -First 1 | Should Be 'Casa: 57368170'
+        agenda-otra-version CONSTANTINO | select -Last 2 | select -First 1 | Should Be 'Casa: 57368170'
+        agenda-otra-version cOnStaNtiNo | select -Last 2 | select -First 1 | Should Be 'Casa: 57368170'
+        agenda-otra-version roberto     | select -Last 2 | select -First 1 | Should Be "El nombre 'roberto' no esta incluido en la agenda"
+    }
+}
