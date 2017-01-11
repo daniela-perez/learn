174e6d849cb6c4119cb0f285a4ca2eb01cd2ee8d
Daniela Perez <daniela@correo.io>
2017-01-10 22:26:14 -0600

    Implementacion de la funcion sumar-n-numeros

---
+++ powershell/sumar-n-numeros.ps1
@@ -0,0 +1,9 @@
+﻿function sumar-n-numeros() {
+    $caracteres = $args + $input
+    $sumatoria  = 0;
+    Foreach ($caracter in $caracteres) {
+    [int]$caracter = [convert]::ToInt32($caracter, 10)
+            $sumatoria += $caracter
+   }
+        "$sumatoria";
+}
