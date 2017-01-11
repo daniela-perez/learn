078363ee23d271ba10898c57c5fa575a20fa471f
Daniela Perez <daniela@correo.io>
2017-01-10 22:07:10 -0600

    Sintetiza la funcion

---
+++ powershell/hibernate-pc.ps1
@@ -0,0 +1,28 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+function Set-PowerState {
+    [CmdletBinding()]
+    param (
+          [System.Windows.Forms.PowerState] $PowerState = [System.Windows.Forms.PowerState]::Suspend
+        , [switch] $DisableWake
+        , [switch] $Forcex
+    )
+
+    begin {
+        if (!$DisableWake) { $DisableWake = $false; };
+        if (!$Force)       { $Force = $false; };
+    }
+
+    process {
+
+        try {
+            $Result = [System.Windows.Forms.Application]::SetSuspendState($PowerState, $Force, $DisableWake);
+        }
+        catch {
+            Write-Error -Exception $_;
+        }
+    }
+}
+
+#Call the function
+Set-PowerState -PowerState Hibernate -DisableWake -Force;
