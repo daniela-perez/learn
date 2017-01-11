50da392d25334fe71e357f2bcbbbb477ee18008c
Daniela Perez <daniela@correo.io>
2017-01-10 22:25:03 -0600

    Implementacion de la funcion sumar-dos-numeros

---
+++ powershell/sumar-dos-numeros.ps1
@@ -0,0 +1,24 @@
+﻿function isNumeric ($x) {
+    try {
+        0 + $x | Out-Null
+        return $true
+    } catch {
+        return $false
+    }
+}
+
+function sumar-dos-numeros($primer_numero, $segundo_numero) {
+     if(!(isNumeric($primer_numero))) { "Error: parametro no numero -> `"$primer_numero`""; return; }
+     if(!(isNumeric($segundo_numero))) { "Error: parametro no numero -> `"$segundo_numero`""; return; }
+
+     $primer_numero = [int]$primer_numero
+     $segundo_numero  = [int]$segundo_numero
+
+     if (isNumeric($primer_numero -and $segundo_numero)) {
+  
+            $sumatoria  = 0;
+            $sumatoria += $primer_numero;
+            $sumatoria += $segundo_numero;
+            "$sumatoria";
+     }           
+}
--- powershell/tests/sumar-dos-numeros.tests.ps1
+++ powershell/tests/sumar-dos-numeros.tests.ps1
@@ -8,5 +8,8 @@ Describe "sumar-dos-numeros" {
         sumar-dos-numeros 1   | Should Be '1'
         sumar-dos-numeros 1 1 | Should Be '2'
         sumar-dos-numeros 5 5 | Should Be '10'
+        sumar-dos-numeros x 5 | Should Be 'Error: parametro no numero -> "x"'
+        sumar-dos-numeros y   | Should Be 'Error: parametro no numero -> "y"'
+        sumar-dos-numeros y x | Should Be 'Error: parametro no numero -> "y"'
     }
 }
